96d98fb19c487ce20cd8464c66ad711e97877fd2
Zacharyyyy <89017645+FoxyZach@users.noreply.github.com>
2023-01-26 20:48:53 +0700

    Update download.py

--- download.py
+++ download.py
@@ -1,11 +1,51 @@
 from tkinter import *
 from tkinter import filedialog
 from moviepy import *
+from moviepy.video.fx.accel_decel import accel_decel
+from moviepy.video.fx.blackwhite import blackwhite
+from moviepy.video.fx.blink import blink
+from moviepy.video.fx.colorx import colorx
+from moviepy.video.fx.crop import crop
+from moviepy.video.fx.even_size import even_size
+from moviepy.video.fx.fadein import fadein
+from moviepy.video.fx.fadeout import fadeout
+from moviepy.video.fx.freeze import freeze
+from moviepy.video.fx.freeze_region import freeze_region
+from moviepy.video.fx.gamma_corr import gamma_corr
+from moviepy.video.fx.headblur import headblur
+from moviepy.video.fx.invert_colors import invert_colors
+from moviepy.video.fx.loop import loop
+from moviepy.video.fx.lum_contrast import lum_contrast
+from moviepy.video.fx.make_loopable import make_loopable
+from moviepy.video.fx.margin import margin
+from moviepy.video.fx.mask_and import mask_and
+from moviepy.video.fx.mask_color import mask_color
+from moviepy.video.fx.mask_or import mask_or
+from moviepy.video.fx.mirror_x import mirror_x
+from moviepy.video.fx.mirror_y import mirror_y
+from moviepy.video.fx.painting import painting
+from moviepy.video.fx.resize import resize
+from moviepy.video.fx.rotate import rotate
+from moviepy.video.fx.scroll import scroll
+from moviepy.video.fx.speedx import speedx
+from moviepy.video.fx.supersample import supersample
+from moviepy.video.fx.time_mirror import time_mirror
+from moviepy.video.fx.time_symmetrize import time_symmetrize
+from moviepy.audio.fx.audio_fadein import audio_fadein
+from moviepy.audio.fx.audio_fadeout import audio_fadeout
+from moviepy.audio.fx.audio_left_right import audio_left_right
+from moviepy.audio.fx.audio_loop import audio_loop
+from moviepy.audio.fx.audio_normalize import audio_normalize
+from moviepy.audio.fx.volumex import volumex
+from moviepy.editor import VideoFileClip
+from pytube import YouTube
+from tkinter import *
+from tkinter import filedialog
+from moviepy import *
 from moviepy.editor import VideoFileClip
 from pytube import YouTube
 
 import shutil
-
 
 #Functions
 def select_path():
@@ -32,13 +72,8 @@ title = screen.title('Youtube Download')
 canvas = Canvas(screen, width=500, height=500)
 canvas.pack()
 
-#image logo
-logo_img = PhotoImage(file='D:\Python\PythonScripts\Projects\yt.png')
-#resize
-logo_img = logo_img.subsample(2, 2)
-canvas.create_image(250, 55, image=logo_img)
-
 #link field
+link_logo = Label(screen, text="YouTube", font=('Arial', 50))
 link_field = Entry(screen, width=40, font=('Arial', 15) )
 link_label = Label(screen, text="Enter Download Link: ", font=('Arial', 15))
 link_description1 = Label(screen, text="Program May Freeze While Downloading!", font=('Arial', 10))
@@ -54,6 +89,7 @@ canvas.create_window(250, 280, window=path_label)
 canvas.create_window(250, 330, window=select_btn)
 
 #Add widgets to window 
+canvas.create_window(250, 80, window=link_logo)
 canvas.create_window(250, 183, window=link_description2)
 canvas.create_window(250, 160, window=link_description1)
 canvas.create_window(250, 135, window=link_label)
